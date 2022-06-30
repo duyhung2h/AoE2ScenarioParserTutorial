09794a51bb63bf743ea35892dba74e13027d1529
duyhung2h <duyhung2h@gmail.com>
2022-06-30 19:15:01 +0700

    them mot vai vi du

--- [Example4] - Transfer trigger to another map/example - how to transfer trigger to another map.py
+++ [Example4] - Transfer trigger to another map/example - how to transfer trigger to another map.py
@@ -1,14 +1,42 @@
+import sys
+import os
 from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
+from AoE2ScenarioParser.datasets.trigger_lists import *
 
-# File & Folder setup - Declare your scenario directory path
-scenario_folder = "C:/Users/Admin/Games/Age of Empires 2 DE/76561198148041091/resources/_common/scenario/"
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(
+        os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+'''
+File & Folder setup
+Copy the file name (click on the line and CTRL + C):
+
+ScenarioParser - RemoveTriggers
+Tales of Tenebria version 0v16
+'''
+
+input_scenario_nanme = input(
+    'Please enter the scenario name (spare the .aoe2scenario extension): ')
+try:
+    scenario_folder = "C:/Users/Admin/Games/Age of Empires 2 DE/76561198148041091/resources/_common/scenario/"
+    source_scenario = AoE2DEScenario.from_file(
+        scenario_folder + input_scenario_nanme + ".aoe2scenario")
+except FileNotFoundError:
+    scenario_folder = resource_path("")
+    print("\ncannot find main scenario folder. redirect to base dir: " + scenario_folder)
 
 # Source scenario to work with
-input_path = scenario_folder + "ScenarioParser - TransferTriggers.aoe2scenario"
-# Transfer scenario to extract triggers into
-transfer_path = scenario_folder + "ScenarioParser - TransferTriggers Transfer.aoe2scenario"
+input_path = scenario_folder + input_scenario_nanme + ".aoe2scenario"
 # New scenario so it wouldn't overwrite the old one
-output_path = scenario_folder + "ScenarioParser - TransferTriggers Parser Result.aoe2scenario"
+output_path = scenario_folder + input_scenario_nanme + \
+    " Parser Result" + ".aoe2scenario"
+# Transfer scenario to extract triggers into
+transfer_path = scenario_folder + input_scenario_nanme + " Transfer.aoe2scenario"
+''''''
 
 # declare scenario class
 source_scenario = AoE2DEScenario.from_file(input_path)
@@ -24,16 +52,20 @@ transfer_trigger_manager = transfer_scenario.trigger_manager
 
 
 # put triggers ranging from index position_start to position_end, to position position_transfer in the target file
-position_start = 0;
-position_end = source_trigger_manager.triggers.__len__();
-position_transfer = -1; #Leaving it out (or -1) is placing your triggers at the end
+position_start = 492;
+# position_end = source_trigger_manager.triggers.__len__();
+position_end = 510;
+position_transfer = 505; #Leaving it out (or -1) is placing your triggers at the end
 
 # Mark the start of trigger copying!
-transfer_trigger_manager.add_trigger(name="---TRIGGER COPY START-----")
+triggersNew = []
+# transfer_trigger_manager.add_trigger(name="---TRIGGER COPY START-----")
+triggersNew.append(source_trigger_manager.triggers[position_start:position_end])
+# transfer_trigger_manager.add_trigger(name="---TRIGGER COPY START-----")
 
 # transfer the triggers and variables
 # (source_trigger_manager.triggers[position_start:position_end]) to get list of triggers from start to end
-transfer_trigger_manager.import_triggers(triggers=source_trigger_manager.triggers[position_start:position_end], index=position_transfer);
+transfer_trigger_manager.import_triggers(triggers=triggersNew, index=position_transfer);
 
 # Final step: write a modified scenario class to a new scenario file
 transfer_scenario.write_to_file(filename=output_path)
